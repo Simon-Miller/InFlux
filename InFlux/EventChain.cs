887b9bf71bd5e902faef205290c1d1d8d0cc1241
Guildor <simonmiller17@hotmail.co.uk>
2022-02-02 22:40:36 +0000

    Got EventChain working, and providing debug information, and even the ability to call a subscriber's code that didn't respond via the callback. NICE!!

--- InFlux/EventChain.cs
+++ InFlux/EventChain.cs
@@ -15,17 +15,16 @@
 
     /// <summary>
     /// Frustrating as it is, neither traditional events, or QueuedEvents offer solid knowledge to the 
-    /// developer of all the order that events are handled in, of in the case of QueuedEvents, whether
+    /// developer of the order that events are handled in, or in the case of QueuedEvents, whether
     /// event subscritions called on have actually processed the event.  
     /// <para>This approach seeks to remedy this.  It uses the QueuedEvents to ensure the ordering,
     /// but also counts all the subscription calls, and passes code that needs to be called when done,
     /// which is tracked as a count.  When all subscriptions report back, it calls its own callback. </para>
     /// <para>In essense then, a chain of events can be triggered where the calls return when done.
-    /// This should translate to a chain of callback when done - </para>
+    /// This should translate to a chain of callbacks when done - </para>
     /// <para>So this should give us the best of both worlds: Predictable event processing, and
     /// knowing when even complicated event chains complete.</para>
     /// </summary>
-    /// <typeparam name="T"></typeparam>
     public class EventChain<T>
     {
         int subId = 0;
@@ -57,38 +56,86 @@
             return false;
         }
 
+        /* both subscribing and unsubscribing during the firing of an event will change the 
+         * subscriptions whilst we are possibly iterating over them, and cause an exception.
+         * Do we duplicate the list before processing?  That sounds sensible.
+         */
+
+        private Dictionary<int, Action<ChainLink<T>>> copySubscriptions()
+        {
+            // Could have used Linq, but this surely would be quicker?
+            // Not that I think performance would be an issue.
+            // Likely only a small number of subscribers at most.
+            var copy = new Dictionary<int, Action<ChainLink<T>>>(this.subscriptions.Count);
+            foreach(var key in this.subscriptions.Keys)
+                copy.Add(key, this.subscriptions[key]);
+
+            return copy;
+        }
+        private List<Action<ChainLink<T>>> copyOneOffSubscriptions()
+        {
+            var copy = new List<Action<ChainLink<T>>>(this.oneOffSubscriptions.Count);
+            foreach(var item in this.oneOffSubscriptions)
+                copy.Add(item);
+
+            return copy;
+        }
+        public record class DebugSubscription(int Id, bool IsOneOff, Action callSubscription);
+        public readonly List<DebugSubscription> DebugSubscriptions = new(); 
+        // TODO: POPULATE THIS ABOVE LIST.
+        // I was thinking that with all subscriptions we generate an index as we fire the event
+        // we generate a DebugSubscription and add to the collection.  As the respond,
+        // we know their ID, and can remove them from the list.
+
         public void FireEvent(T payload, Action callbackWhenDone)
         {
-            int handlersCount = this.subscriptions.Count + this.oneOffSubscriptions.Count;
-            if (handlersCount <= 0)
+            var subscriptionsCount = this.subscriptions.Count + this.oneOffSubscriptions.Count;
+            if (subscriptionsCount <= 0)
             {
                 callbackWhenDone();
                 return;
             }
 
-            foreach (var subscription in this.subscriptions)
+            this.DebugSubscriptions.Clear();
+            var nextDebugId = 1;
+
+            var iterableSubscriptions = this.copySubscriptions();
+            foreach (var subscription in iterableSubscriptions)
             {
-                QueuedActions.Add(() => 
-                {
-                    subscription.Value.Invoke(new ChainLink<T>(payload, callbackFromSubscription));
-                });
+                ChainLink<T> subscriberCallback = generatePayloadForSubscriber(nextDebugId);
+                var debugRow = new DebugSubscription(nextDebugId, false, 
+                                    () => subscription.Value.Invoke(subscriberCallback));
+
+                this.DebugSubscriptions.Add(debugRow);
+                nextDebugId++;
+
+                QueuedActions.Add(() => subscription.Value.Invoke(subscriberCallback));
             }
 
-            foreach (var subscription in this.oneOffSubscriptions)
+            var iterableOneOffSubscriptions = this.copyOneOffSubscriptions();
+            foreach (var subscription in iterableOneOffSubscriptions)
             {
-                QueuedActions.Add(() => 
-                {
-                    subscription.Invoke(new ChainLink<T>(payload, callbackFromSubscription));
-                });
+                ChainLink<T> subscriberCallback = generatePayloadForSubscriber(nextDebugId);
+                var debugRow = new DebugSubscription(nextDebugId, true,
+                                    () => subscription.Invoke(subscriberCallback));
+
+                this.DebugSubscriptions.Add(debugRow);
+                nextDebugId++;
+
+                QueuedActions.Add(() => subscription.Invoke(subscriberCallback));
             }
 
             this.oneOffSubscriptions.Clear();
 
-            void callbackFromSubscription()
-            {
-                handlersCount--;
+            ChainLink<T> generatePayloadForSubscriber(int debugId) => 
+                new ChainLink<T>(payload, ()=> callbackFromSubscription(debugId));
+            
+            void callbackFromSubscription(int debugId)
+            {            
+                this.DebugSubscriptions.Remove(this.DebugSubscriptions.First(x => x.Id == debugId));
+                subscriptionsCount--;
 
-                if (handlersCount <= 0)
+                if (subscriptionsCount <= 0)
                     callbackWhenDone();
             }
         }
--- InFluxTests/EventChainTests.cs
+++ InFluxTests/EventChainTests.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InFluxTests
+﻿namespace InFluxTests
 {
     [TestClass]
     public class EventChainTests
@@ -19,19 +12,19 @@ namespace InFluxTests
             var oneOff = 0;
             var inst1 = new testClass();
             var inst2 = new testClass();
-            inst1.Event.Subscribe(cl => 
+            inst1.Event.Subscribe(cl =>
             {
-                inst2.Event.FireEvent(123, ()=> 
+                inst2.Event.FireEvent(123, () =>
                 {
                     cl.callbackWhenDone(); 
                 });
             });
-            var subKey = inst2.Event.Subscribe(cb => 
+            var subKey = inst2.Event.Subscribe(cb =>
             {
-                innerCalls++; 
-                cb.callbackWhenDone(); 
+                innerCalls++;
+                cb.callbackWhenDone();
             });
-            inst2.Event.SubscribeOnce(cb=> 
+            inst2.Event.SubscribeOnce(cb =>
             {
                 oneOff++;
                 cb.callbackWhenDone();
@@ -41,6 +34,21 @@ namespace InFluxTests
 
             // FAIL!!!   This isn't calling outercalls++, but I can't see why.
             inst1.Event.FireEvent(321, () => outerCalls++);
+
+            if (inst1.Event.DebugSubscriptions.Count > 0)
+            {
+                // debug showing some kind of error in chain.
+                inst1.Event.DebugSubscriptions[0].callSubscription();
+
+                /* There's no way of knowing exactly, and repeated calls to the event causes
+                 * it to reset each time.  Therefore you can only test a chain immediately after
+                 * it supposedly completes. It won't build up a memory of all the subscriptions ever
+                 * called, and not heard back from!  Just over the firing of the event.
+                 * But still, having the ability to call the code that didn't respond, and see it 
+                 * and debug it, is a big help!
+                 */
+            }
+
             inst1.Event.FireEvent(432, () => outerCalls++);
 
             inst2.Event.Unsubscribe(subKey);
