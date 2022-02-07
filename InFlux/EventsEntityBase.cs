ea0cb24734a818ec8ef8d6a5a78683c8ddac5483
Guildor <simonmiller17@hotmail.co.uk>
2022-02-07 21:55:21 +0000

    EventsEntityBase now listens to ChainEvent types, and reports when entity changes for these field/propertytypes.  Unit test supported.

---
+++ InFlux/EventsEntityBase.cs
@@ -0,0 +1,172 @@
+﻿using System.Reflection;
+
+namespace InFlux
+{
+    /// <summary>
+    /// represents an entity that shouts about member values changing.
+    /// Automatically signs up to value change events on the inheritor's members
+    /// of type <see cref="QueuedEventProperty{T}"/>,
+    /// and type <see cref="QueuedEventList{T}"/>
+    /// </summary>
+    public abstract class EventsEntityBase<T> where T : EventsEntityBase<T>
+    {
+        public EventsEntityBase()
+        {
+            // scan self for members inheriting QueuedEventPropertyBase;
+
+            // NOTE commented out NonPublic, as backing-fields get picked up as well as public properties.
+
+            var genericFieldMembers = this.GetType()
+                    .GetFields(BindingFlags.Public
+                             | BindingFlags.Instance)
+                    .Where(x => x.FieldType.IsGenericType)
+                    .ToList();
+
+            var genericPropertyMembers = this.GetType()
+                    .GetProperties(BindingFlags.Public
+                                 | BindingFlags.Instance)
+                    .Where(x => x.PropertyType.IsGenericType)
+                    .ToList();
+
+            genericFieldMembers.Where(f => f
+                .FieldType.IsAssignableTo(typeof(QueuedEventPropertyBase)))
+                .Each(member => this.setupMember(member.GetValue(this)));
+
+            genericPropertyMembers.Where(p => p
+                .PropertyType.IsNestedAssembly && p
+                .PropertyType.IsAssignableTo(typeof(QueuedEventPropertyBase)))
+                    .Each(member => this.setupMember(member.GetValue(this)));
+
+            genericFieldMembers
+                .Where(f => f.FieldType.GetGenericTypeDefinition() == typeof(QueuedEventList<>))
+                .Each(member => this.setupListMember(member.GetValue(this)));
+
+            genericPropertyMembers.Where(p => p
+                .PropertyType.GetGenericTypeDefinition() == typeof(QueuedEventList<>))
+                    .Each(member => this.setupListMember(member.GetValue(this)));
+
+            genericFieldMembers
+                .Where(f => f.FieldType.GetGenericTypeDefinition() == typeof(EventChainProperty<>))
+                .Each(member => this.setupEventChainPropertyMember(member.GetValue(this)));
+
+            genericPropertyMembers.Where(p => p
+                .PropertyType.GetGenericTypeDefinition() == typeof(EventChainProperty<>))
+                .Each(member => this.setupEventChainPropertyMember(member.GetValue(this)));
+
+            genericFieldMembers
+                .Where(f => f.FieldType.GetGenericTypeDefinition() == typeof(EventChainList<>))
+                .Each(member => this.setupEventChainListMember(member.GetValue(this)));
+
+            genericPropertyMembers.Where(p => p
+                .PropertyType.GetGenericTypeDefinition() == typeof(EventChainList<>))
+                .Each(member => this.setupEventChainListMember(member.GetValue(this)));
+        }
+
+        /// <summary>
+        /// should fire when any <see cref="QueuedEventProperty{T}"/> member of this instance changes.
+        /// The idea being you create your own strongly typed event where you can pass on your entity
+        /// to your subscribers so they know which entity changed.
+        /// </summary>
+        public readonly QueuedEvent<T> EntityChanged = new();
+
+        //something isn't right here.  or setupMember is wrong too.
+        private void onMemberValueChanged(object? oldValue, object? newValue) => this.EntityChanged.FireEvent(default, (T?)this);
+
+        private void setupMember(object? member)
+        {
+            if (member != null)
+                ((QueuedEventPropertyBase)member).OnValueChanged((oldValue, newValue) =>
+                    this.onMemberValueChanged(oldValue, newValue));
+        }
+
+        private void setupListMember(object? member)
+        {
+            if (member != null)
+            {
+                // most of the time I love reflection!  FEEL THE FORCE, LUKE!
+                // but sometimes, I hate it!  Its taken about 2 hours to get this call to work.
+                // truly, jumping through hoops.
+
+                var memberType = member.GetType();
+                var genericArg = memberType.GetGenericArguments()[0];
+
+                var thisGenArgType = this.GetType()?.BaseType?.GenericTypeArguments[0];
+
+                var thisFUllType = typeof(EventsEntityBase<>).MakeGenericType(thisGenArgType!);
+
+                var invoker = thisFUllType!.GetMethod(
+                                 nameof(setupListMemberGeneric),
+                                 BindingFlags.Instance | BindingFlags.NonPublic);
+
+                var genericInvoker = invoker!.MakeGenericMethod(genericArg);
+
+                genericInvoker.Invoke(this, new object[] { member });
+            }
+        }
+
+        private void setupListMemberGeneric<T2>(QueuedEventList<T2> member) =>
+            member?.OnListChanged.Subscribe((O, N) => this.EntityChanged.FireEvent(default, (T?)this));
+
+        private void setupEventChainListMember(object? member) =>
+            commonSetupEventChainOrPropMember(member,
+                                              () => typeof(EventChainList<>),
+                                              nameof(setupEventChainListMemberGeneric));
+        //if (member != null)
+        //{
+        //    var memberType = member.GetType();
+        //    var genericArg = memberType.GetGenericArguments()[0];
+
+        //    var thisGenArgType = this.GetType()?.BaseType?.GenericTypeArguments[0];
+
+        //    var thisFUllType = typeof(EventChain<>).MakeGenericType(thisGenArgType!);
+
+        //    var invoker = thisFUllType!.GetMethod(
+        //                     nameof(setupEventChainMemberGeneric),
+        //                     BindingFlags.Instance | BindingFlags.NonPublic);
+
+        //    var genericInvoker = invoker!.MakeGenericMethod(genericArg);
+
+        //    genericInvoker.Invoke(this, new object[] { member });
+        //}
+
+
+        private void commonSetupEventChainOrPropMember(object? member, Func<Type> getEventChainOrPropGenericType, string nameOfGenericSetupMethod)
+        {
+            if (member != null)
+            {
+                var memberType = member.GetType();
+                var genericArg = memberType.GetGenericArguments()[0];
+
+                var thisGenArgType = this.GetType()?.BaseType?.GenericTypeArguments[0];
+
+                //var thisFUllType = getEventChainOrPropGenericType().MakeGenericType(thisGenArgType!);
+
+                var invoker = this.GetType()?.BaseType?.GetMethod(
+                                 nameOfGenericSetupMethod,
+                                 BindingFlags.Instance | BindingFlags.NonPublic);
+
+                var genericInvoker = invoker!.MakeGenericMethod(genericArg);
+
+                genericInvoker.Invoke(this, new object[] { member });
+            }
+        }
+
+        private void setupEventChainListMemberGeneric<T2>(EventChainList<T2> member)
+        {
+            member.OnListChanged.Subscribe(chain =>
+            {
+                chain.callbackWhenDone();
+                this.EntityChanged.FireEvent(default, (T?)this);
+            });     
+        }
+
+        private void setupEventChainPropertyMember(object? member) =>
+            commonSetupEventChainOrPropMember(member, 
+                                              ()=> typeof(EventChainProperty<>), 
+                                              nameof(setupEventChainPropertyMemberGeneric));
+
+        private void setupEventChainPropertyMemberGeneric<T2>(EventChainProperty<T2> member) =>        
+            member.ValueChangedEventCompleted.Subscribe(() =>
+                this.EntityChanged.FireEvent(default, (T?)this));      
+    }
+}
--- InFluxTests/EventsEntityBaseTests.cs
+++ InFluxTests/EventsEntityBaseTests.cs
@@ -1,7 +1,7 @@
 ﻿namespace InFluxTests
 {
     [TestClass]
-    public class QueuedEventsEntityTests
+    public class EventsEntityBaseTests
     {
         [TestMethod]
         public void Can_listen_to_entity_changed()
@@ -17,9 +17,13 @@
             inst.Age.Value = 103;
             inst.ItemCodes.Add(123);
             inst.ListOfBools.Add(true);
+            inst.ECPropField.Value = 1234;
+            inst.ECListField.Add(4321);
+            inst.ECPropProp.Value = 111;
+            inst.ECListProp.Add(true);
 
             // assert
-            Assert.AreEqual(3,calledCount);
+            Assert.AreEqual(7,calledCount);
         }
     }
 
@@ -30,13 +34,27 @@
     /// That means you'd need to define a getter / setter property of your own,
     /// and have the value itself validate but get its value from an event property?
     /// </summary>
-    class Entity : QueuedEventsEntity<Entity>
+    class Entity : EventsEntityBase<Entity>
     {
+        // field
         public readonly QueuedEventProperty<int> Age = new(initialValue: 0, onlyFireOnValueChanges: true);
 
+        // field
         public readonly QueuedEventList<int> ItemCodes = new();
 
-        // this matches what I've done in real app.
+        // property
         public QueuedEventList<bool> ListOfBools { get; init; } = new();
+
+        // field
+        public readonly EventChainProperty<int> ECPropField = new();
+
+        // field
+        public readonly EventChainList<int> ECListField = new();
+
+        // property
+        public EventChainProperty<int> ECPropProp { get; set; } = new();
+
+        // property
+        public EventChainList<bool> ECListProp { get; set; } = new();
     }
 }
