fd718f050faff990f3b5497e6a7a1c35a45db261
Guildor <simonmiller17@hotmail.co.uk>
2022-02-10 07:27:41 +0000

    NuGet improvents

--- InFluxTests/EventsSuckTests.cs
+++ InFluxTests/EventsSuckTests.cs
@@ -10,7 +10,7 @@
         public void Ordering_of_events_not_as_expected()
         {
             // Arrange
-            Events.List.Clear();
+            Notes.List.Clear();
 
             var hotel = new Hotel();
             var fireBrigade = new FireBrigade();
@@ -25,22 +25,22 @@
 
             // Assert
             // you would perhaps innocently expect the sequence to look like this:
-            Assert.IsTrue(Events.List[0] == "hotel: Someone help! There's a fire!");
-            Assert.IsTrue(Events.List[1] == "News flash! Hotel on fire!");
-            Assert.IsTrue(Events.List[2] == "fire brigade: On our way!");
-            Assert.IsTrue(Events.List[3] == "News flash! Fire fighters fight fire!");
+            Assert.IsTrue(Notes.List[0] == "hotel: Someone help! There's a fire!");
+            Assert.IsTrue(Notes.List[1] == "News flash! Hotel on fire!");
+            Assert.IsTrue(Notes.List[2] == "fire brigade: On our way!");
+            Assert.IsTrue(Notes.List[3] == "News flash! Fire fighters fight fire!");
 
             // Maybe you're expecting this sequence:
-            Assert.IsTrue(Events.List[0] == "hotel: Someone help! There's a fire!");
-            Assert.IsTrue(Events.List[2] == "fire brigade: On our way!");
-            Assert.IsTrue(Events.List[1] == "News flash! Hotel on fire!");
-            Assert.IsTrue(Events.List[3] == "News flash! Fire fighters fight fire!");
+            Assert.IsTrue(Notes.List[0] == "hotel: Someone help! There's a fire!");
+            Assert.IsTrue(Notes.List[2] == "fire brigade: On our way!");
+            Assert.IsTrue(Notes.List[1] == "News flash! Hotel on fire!");
+            Assert.IsTrue(Notes.List[3] == "News flash! Fire fighters fight fire!");
 
             // What you actually get, is this:
-            Assert.IsTrue(Events.List[0] == "hotel: Someone help! There's a fire!");
-            Assert.IsTrue(Events.List[2] == "fire brigade: On our way!");
-            Assert.IsTrue(Events.List[3] == "News flash! Fire fighters fight fire!");
-            Assert.IsTrue(Events.List[1] == "News flash! Hotel on fire!");
+            Assert.IsTrue(Notes.List[0] == "hotel: Someone help! There's a fire!");
+            Assert.IsTrue(Notes.List[2] == "fire brigade: On our way!");
+            Assert.IsTrue(Notes.List[3] == "News flash! Fire fighters fight fire!");
+            Assert.IsTrue(Notes.List[1] == "News flash! Hotel on fire!");
 
             // NOTE: The reporter reports on the fire being put out BEFORE reporting on the fire.
             // When we develop against events, this is the hardest thing to understand.
@@ -53,7 +53,7 @@
         public event Action? KitchenOnFire;
         public void StartFire()
         {
-            Events.List.Add("hotel: Someone help! There's a fire!");
+            Notes.Add("hotel: Someone help! There's a fire!");
             KitchenOnFire?.Invoke();
         }
     }
@@ -62,17 +62,21 @@
         public event Action? PuttingOutAFire;
         public void PutOutFire()
         {
-            Events.List.Add("fire brigade: On our way!");
+            Notes.Add("fire brigade: On our way!");
             PuttingOutAFire?.Invoke();
         }
     }
     class Reporter
     {
-        public void ReportOnHotelFire() => Events.List.Add("News flash! Hotel on fire!");
-        public void ReportOnFireBrigade() => Events.List.Add("News flash! Fire fighters fight fire!");
+        public void ReportOnHotelFire() => 
+            Notes.Add("News flash! Hotel on fire!");
+
+        public void ReportOnFireBrigade() => 
+            Notes.Add("News flash! Fire fighters fight fire!");
     }
-    static class Events
+    static class Notes
     {
         public static readonly List<string> List = new();
+        public static void Add(string note) => List.Add(note);
     }
 }
