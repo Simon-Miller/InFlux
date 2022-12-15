b54d9ec9654d1a3ef80df46696ce4b14f7947b3d
ITSSMILL@ljmu.ac.uk <ITSSMILL@ljmu.ac.uk>
2022-12-15 12:28:44 +0000

    Added some new classes to help with a new way to deal with a shouty-class, with shouty-properties, but contain insights into the state of the model  (dirty, untouched).  It feels like validation can and should happen independently, but was considered.

---
+++ InFlux/Insights.cs
@@ -0,0 +1,60 @@
+﻿namespace InFlux
+{
+    /// <summary>
+    /// Indirect metadata about a property associated with this instance.  Likey similarly named.
+    /// </summary>
+    public class Insights<T>
+    {
+        /// <summary>
+        /// To construct an Insights instance, you need to provide the <paramref name="intent"/> and a starting value
+        /// from which any further metadata can be derived.
+        /// </summary>
+        public Insights(IntentProcessor intent, T startValue)
+        {
+            Intent = intent;
+            OriginalValueHash = startValue?.GetHashCode() ?? 0;
+        }
+
+        internal T PreviousValue = default;
+        internal int OriginalValueHash;
+
+        /// <summary>
+        /// Indicates if the target property's value ever changes.  It it does, it's considered to 
+        /// be Touched upon by the user or system.  Can be reset by calling <see cref="ResetToPristine"/>
+        /// </summary>
+        public bool IsTouched { get; internal set; } = false;
+
+        /// <summary>
+        /// Indicates is the current value for the target property differs from the original value.
+        /// Its possible for the value to change, and then change back to the original value, in which case
+        /// this property would consider the current value to be 'clean', where as any change from the 
+        /// original value implies it is not 'dirty' as a result of the change.  This can be reset by
+        /// calling <see cref="ResetToPristine"/>
+        /// </summary>
+        public bool IsDirty { get; internal set; } = false;
+
+        /// <summary>
+        /// If you want to be informed when the target property's value changes, this is the event for you!
+        /// </summary>
+        public QueuedEvent<T> OnValueChanged = new QueuedEvent<T>();
+
+        /// <summary>
+        /// Fires when the anticipated change to the value was cancelled by the user via an Intent.
+        /// </summary>
+        public QueuedEvent<T> OnValueUnChanged = new QueuedEvent<T>();
+
+        internal readonly IntentProcessor Intent;
+
+        /// <summary>
+        /// You might want to cause this Insight to reset after performing an action such as saving data,
+        /// where the current data should now be considered a source of truth.  This means the target property's
+        /// value will be considered both untouched, and clean. <see cref="IsTouched"/> will report FALSE,
+        /// and <see cref="IsDirty"/> will also report FALSE.
+        /// </summary>
+        public void ResetToPristine()
+        {
+            IsTouched = false;
+            IsDirty = false;
+        }
+    }
+}
---
+++ InFlux/Intent.cs
@@ -0,0 +1,75 @@
+﻿namespace InFlux
+{
+    /// <summary>
+    /// Represents the intent to change a value.
+    /// The Intent Processor allows any subscribers to the intent,
+    /// to participate in a find of 'validation' process in which you may need
+    /// permission from the user to make the change.  If nothing listening to
+    /// the intent thinks the user needs be be asked, (no unsaved changes?)
+    /// then the intended change can be executed by the Intent Processor.
+    /// <para>
+    /// This object is intended to be passed around via the subscribed event,
+    /// giving code change to call the <see cref="AskUserForPermission"/> methor,
+    /// OR NOT.  
+    /// </para>
+    /// </summary>
+    public class Intent<T>
+    {
+        /// <summary>
+        /// construct an externally immutable instance containing the values provided.
+        /// </summary>
+        public Intent(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        /// <summary>
+        /// The current value that may be lost if the intended change goes ahead
+        /// </summary>
+        public T OldValue { get; private set; }
+
+        /// <summary>
+        /// The new value that will replace the <see cref="OldValue"/> in the target object,
+        /// should there be no objection from any subscribers to the Intent process, or
+        /// from the user.
+        /// </summary>
+        public T NewValue { get; private set; }
+
+        internal bool PermissionNeededForChange { get; private set; }
+
+        /// <summary>
+        /// If the intended change could cause the user to lose data (unsaved changes?)
+        /// Then callis this will set an internal state that the Intent Processor will respond to,
+        /// and in turn fire off a application level event that if implemented by the UI,
+        /// will trigger a request from the user to verify that they are ok with losing any
+        /// unsaved changes.  If they are not, they should have the option to cancel.
+        /// The user response should feed back into the Intent Processor, and based on the user
+        /// agreeing, the code that performs the intended change is executed OR NOT.
+        /// </summary>
+        public void AskUserForPermission()
+        {
+            PermissionNeededForChange = true;
+        }
+
+        /// <summary>
+        /// Compares this instance to other object using VALUES.
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            var other = obj as Intent<T>;
+            if (other is null) return false;
+
+            if (OldValue.Equals(other.OldValue) == false) return false;
+            if (NewValue.Equals(other.NewValue) == false) return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Generate a hashcode to represent this instance's values.
+        /// </summary>
+        public override int GetHashCode() =>
+            OldValue.GetHashCode() ^ NewValue.GetHashCode();
+    }
+}
